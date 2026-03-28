9f28cc815752e7c2d596fa4bd7cf6f98e1940729
juliolmuller <juliolmuller@outlook.com>
2026-03-28 11:33:33 -0300

    feat(cpf-dv): add method to return error/exception name

--- src/Exceptions/CpfCheckDigitsException.php
+++ src/Exceptions/CpfCheckDigitsException.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Lacus\BrUtils\Cpf\Exceptions;
 
 use Exception;
+use ReflectionClass;
 
 /**
  * Base exception for all `cpf-dv` rules-related errors.
@@ -16,4 +17,18 @@ use Exception;
  */
 abstract class CpfCheckDigitsException extends Exception
 {
+    public function __construct(string $message)
+    {
+        parent::__construct($message);
+    }
+
+    /**
+     * Get the name of the class instance name.
+     */
+    public function getName(): string
+    {
+        $thisReflection = new ReflectionClass($this);
+
+        return $thisReflection->getShortName();
+    }
 }
